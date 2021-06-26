28f6d98c887d65af12cdc633ed29eac55db045cd
= <=>
2021-06-26 16:22:01 +0200

    my first commit

---
+++ introduccion/prueba.php
@@ -0,0 +1,4 @@
+<?php
+echo "Hola Mundo";
+?>
+<!-- Per veure la cadena impresa, posem al navegador: http://localhost/cursos/introduccion/prueba.php -->
---
+++ introduccion/variables.php
@@ -0,0 +1,14 @@
+<?php
+$algun_nombre = "Hola";
+$Algun_nombre = "Mundo";
+
+echo $algun_nombre . " " . $Algun_nombre;
+/* se concatena con puntos */
+
+$cadena = "Soy una cadena";
+$numero = 4;
+$flotante = 4.4;
+$caracter = 'C';
+/* en php no es necesario declarar el tipo de dato */
+echo $cadena;
+?>
