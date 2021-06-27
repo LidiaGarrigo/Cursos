ec4ad53729876568d3f454da6a3b1dc8bde2b6e9
= <=>
2021-06-27 10:30:30 +0200

    operadores

---
+++ Udemy/PHP/introduccion/operadores.php
@@ -0,0 +1,69 @@
+<?php
+
+//Operadores Logicos
+// > < >= <= <> != ==
+
+$a = 1;
+$b = 2;
+
+//Mayor que >
+if ($a > $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+echo "<br>";
+
+//Menor que <
+if ($a < $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+echo "<br>";
+
+//Mayor o igual que >=
+if ($a >= $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+echo "<br>";
+
+//Menor o igual que <=
+if ($a <= $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+echo "<br>";
+
+//Diferente <> - !=
+if ($a <> $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+echo "<br>";
+
+if ($a != $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+echo "<br>";
+
+//Igual ==
+if ($a == $b){
+    echo "Verdadero";
+} else {
+    echo "Incorrecto";
+}
+
+?>
