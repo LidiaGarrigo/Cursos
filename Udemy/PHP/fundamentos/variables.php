a86cfc244cc06924a3d4b1f5ff4e392ef5d25920
= <=>
2021-06-28 12:14:59 +0200

    fundamentos

---
+++ Udemy/PHP/fundamentos/variables.php
@@ -0,0 +1,22 @@
+<?php
+
+# String: Cadena de texo
+$nombre = "Lidia";
+
+# Integer: Numeros enteros
+$numero = 3;
+
+# Double: Numeros con decimales
+$numero_decimal = 3.3;
+
+# Boolean: Verdadero o falso (true/false)
+$verdadero = false;
+
+# Array
+# Object
+# Class
+# Null: Cuando a una variable aun no se le ha asignado ningun valor
+
+echo gettype($nombre);
+
+?>
