8c4e51c6e9b800f785b36ab91e48a81786acbc84
= <=>
2021-06-29 18:11:17 +0200

    fundamentos

---
+++ Udemy/PHP/fundamentos/array_asociativo.php
@@ -0,0 +1,11 @@
+<?php
+
+$alex = array('telefono' => '9654954', 'edad' => 25, 'apellido' => 'Garcia', 'pais' => 'Francia');
+
+echo 'La edad de Alex es: '.$alex['edad'].'<br>';
+
+//Cambiamos valores del array:
+$alex['telefono'] = 687698;
+echo 'Nuevo telefono de Alex: '.$alex['telefono'];
+
+?>
