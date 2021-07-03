f17a29c674df6a5d4ff35b67e80d40580a0efe7a
= <=>
2021-07-03 13:11:10 +0200

    fundamentos

---
+++ Udemy/PHP/fundamentos/array_multidimensional.php
@@ -0,0 +1,13 @@
+<?php
+
+$amigos = array (
+    array('Alejandro', 20), 
+    array('Maria', 21), 
+    array('Judit', 18)
+);
+
+echo $amigos[0][0] . '<br>';
+echo $amigos[1][0] . '<br>';
+echo $amigos[2][0] . '<br>';
+
+?>
