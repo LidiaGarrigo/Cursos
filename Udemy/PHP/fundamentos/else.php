95179b6713b11c6ebc06121ce2b74be0412c7bd1
= <=>
2021-07-05 15:56:24 +0200

    fundamentos

---
+++ Udemy/PHP/fundamentos/else.php
@@ -0,0 +1,23 @@
+<?php
+
+/* $edad = 18;
+
+if($edad >= 18){
+    echo "Bienvenid@";
+} else {
+    echo "No eres mayor de edad";
+} */
+
+$mes = 'Enero';
+
+if ($mes == 'Diciembre') {
+    echo "¡Feliz Navidad!";
+} else if ($mes == 'Enero'){
+    echo "¡Feliz Año Nuevo!";
+} elseif ($mes == 'Julio') {
+    echo "¡Feliz Julio!";
+} else {
+    echo "El mes introducido no tiene celebración.";
+}
+
+?>
